aeb1bb61215927ae2a46d169974203a1121e5302
chakirach <83536488+chakirach@users.noreply.github.com>
2023-08-05 14:58:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -29,10 +29,17 @@ def read_root():
   
 @app.get("/gotolink2/{item_id}")
 def gotolink2(item_id: str, q: Union[str, None] = None):
-    link=channelsearch(str(item_id))
-    print("hello")
-    print(link)
-    return link
+    url = "https://all-media-downloader.p.rapidapi.com/download"
+    print(item_id)
+    payload = { "url": item_id }
+    headers = {
+    	"content-type": "application/x-www-form-urlencoded",
+    	"X-RapidAPI-Key": "a3e5a1eba4mshaac9e2bcc2d9949p1eec49jsn6230f553eb03",
+    	"X-RapidAPI-Host": "all-media-downloader.p.rapidapi.com"
+    }
+    response = requests.post(url, data=payload, headers=headers)
+    jess_dict2 = json.loads(response.text)
+    return {jess_dict2}
 
 @app.get("/gotolink/{item_id}")
 async def gotolink(item_id: str, q: Union[str, None] = None):
